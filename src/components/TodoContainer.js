5d095fbeecac196897d9112e4fa9475f65a751b2
rpire <rubenpire7@gmail.com>
2021-09-20 06:26:27 -0400

    Set up repository

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
+      </>
+    );
+  }
+};
+
+export default TodoContainer;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
+
+ReactDOM.render(
+  <react.StrictMode>
+    <TodoContainer />
+  </react.StrictMode>,
+  document.getElementById('root')
+  );
