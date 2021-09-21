344f84a63a86f2b766b5211d930619911d01e0cb
rpire <rubenpire7@gmail.com>
2021-09-21 11:54:28 -0400

    Keep functional components and hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import TodoContainer from './functionBased/components/TodoContainer';
-import './functionBased/App.css';
+import TodoContainer from './components/TodoContainer';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
