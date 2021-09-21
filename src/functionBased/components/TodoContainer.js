e31e554a827a723a12e4efec7eaa3fb4a3d28fe7
rpire <rubenpire7@gmail.com>
2021-09-21 04:54:15 -0400

    Format TodoContainer.js

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,49 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import Header from './Header';
-import InputTodo from './InputTodo';
-import TodosList from './TodosList';
-import Navbar from './Navbar';
-import About from '../pages/About';
-import NotMatch from '../pages/NotMatch';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import Header from "./Header";
+import InputTodo from "./InputTodo";
+import TodosList from "./TodosList";
+import Navbar from "./Navbar";
+import About from "../pages/About";
+import NotMatch from "../pages/NotMatch";
 
 const TodoContainer = () => {
   const getInitialTodos = () => {
-    const temp = localStorage.getItem('todos');
+    const temp = localStorage.getItem("todos");
     const defaultTodos = [
       {
         id: uuidv4(),
-        title: 'Add your first todo',
+        title: "Add your first todo",
         completed: false,
       },
     ];
     const savedTodos = JSON.parse(temp);
-    return savedTodos || defaultTodos; 
+    return savedTodos || defaultTodos;
   };
 
   const [todos, setTodos] = useState(getInitialTodos());
 
   const handleChange = (id) => {
     setTodos((prevState) =>
-      prevState.todos.map((todo) => {
+      prevState.map((todo) => {
         if (todo.id === id) {
           return {
             ...todo,
             completed: !todo.completed,
-          }
+          };
         }
         return todo;
-      }),
+      })
     );
   };
 
   const delTodo = (id) => {
-    setTodos([
-      ...todos.filter((todo) => {
-        return todo.id !== id;
-      })
-    ])
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
   };
 
   const addTodoItem = (title) => {
@@ -52,7 +48,7 @@ const TodoContainer = () => {
       title,
       completed: false,
     };
-    setTodos([...todos, newTodo])
+    setTodos([...todos, newTodo]);
   };
 
   const setUpdate = (updatedTitle, id) => {
@@ -72,34 +68,34 @@ const TodoContainer = () => {
 
   useEffect(() => {
     const temp = JSON.stringify(todos);
-    localStorage.setItem('todos', temp);
+    localStorage.setItem("todos", temp);
   }, [todos]);
 
   return (
     <>
-    <Navbar />
-    <Switch>
-      <Route exact path="/">
-        <div className="container">
-          <div className="inner">
-            <Header />
-            <InputTodo addTodoProps={addTodoItem} />
-            <TodosList
-              todos={todos}
-              handleChangeProps={handleChange}
-              deleteTodoProps={delTodo}
-              setUpdate={setUpdate}
-            />
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
           </div>
-        </div>
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="*">
-        <NotMatch />
-      </Route>
-    </Switch>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
     </>
   );
 };
