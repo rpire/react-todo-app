1e122ffa82a12c505271555f26059fc6addce741
rpire <rubenpire7@gmail.com>
2021-09-21 05:10:58 -0400

    Fix indentation in TodoContainer.js

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
 import InputTodo from './InputTodo';
-import TodosList from '/TodosList';
+import TodosList from './TodosList';
 import Navbar from './Navbar';
 import About from '../pages/About';
 import NotMatch from '../pages/NotMatch';
@@ -26,14 +26,14 @@ const TodoContainer = () => {
 
   const handleChange = (id) => {
     setTodos((prevState) => prevState.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          };
-        }
-        return todo;
-      }));
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    }));
   };
 
   const delTodo = (id) => {
